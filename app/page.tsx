5dbe6db38cfa5d63eb35b3aa863d60f908b40a2d
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-03-06 13:09:35 +0100

    oida voi org neich framework
    https://motion.dev/

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Navbar from "@/app/tsx/navbar";
 import StadthalleImage from './pictures/Stadthalle.png';
 import Logo from './pictures/szl-pfp-transparent_border.png'
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 export default function Home() {
     return (
@@ -16,12 +17,37 @@ export default function Home() {
                 }}
             >
                 <div className="flex flex-col md:flex-row justify-between items-center h-full">
-                    <h1 className=" text-9xl font-black text-white pl-8 md:pl-40 float-left drop-shadow-md">
-                        <span className="block">SZ-</span>
-                        <span className="block">LÄUFT</span>
-                    </h1>
-                    <div className="relative mt-4 md:mt-0 md:pr-40 float-right">
-                        <div className=" rounded-full p-3">
+                    <motion.h1
+                        className="text-9xl font-black text-white pl-8 md:pl-40 drop-shadow-md"
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.8, ease: "easeInOut" }}
+                    >
+                        <motion.span
+                            className="block"
+                            initial={{ opacity: 0, x: -50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.8, ease: "easeInOut", delay: 0.2 }}
+                        >
+                            SZ-
+                        </motion.span>
+                        <motion.span
+                            className="block"
+                            initial={{ opacity: 0, x: 50 }}
+                            animate={{ opacity: 1, x: 0 }}
+                            transition={{ duration: 0.8, ease: "easeInOut", delay: 0.4 }}
+                        >
+                            LÄUFT
+                        </motion.span>
+                    </motion.h1>
+
+                    <motion.div
+                        className="relative mt-4 md:mt-0 md:pr-40"
+                        initial={{ opacity: 0, scale: 0.8 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        transition={{ duration: 0.8, ease: "easeInOut", delay: 0.6 }}
+                    >
+                        <div className="rounded-full p-3">
                             <Image
                                 src={Logo}
                                 alt="Logo"
@@ -30,7 +56,7 @@ export default function Home() {
                                 className="w-48 h-48 sm:w-64 sm:h-64 md:w-80 md:h-80 lg:w-96 lg:h-96 xl:w-[400px] xl:h-[400px] drop-shadow-md"
                             />
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </>
