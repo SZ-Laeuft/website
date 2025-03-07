d869750431978676db7c5c1cd6bcdc8907d81b85
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-03-07 07:44:19 +0100

    new animation speed

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,12 @@ import { motion } from "framer-motion";
 
 const textVariants = {
     hidden: { opacity: 0, y: 50 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeInOut" } }
+    visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeInOut" } }
 };
 
 const spanVariants = (direction: number) => ({
     hidden: { opacity: 0, x: direction * 50 },
-    visible: { opacity: 1, x: 0, transition: { duration: 0.8, ease: "easeInOut", delay: 0.2 } }
+    visible: { opacity: 1, x: 0, transition: { duration: 0.7, ease: "easeInOut", delay: 0.2 } }
 });
 
 export default function Home() {
@@ -44,7 +44,7 @@ export default function Home() {
                         className="relative"
                         initial={{ opacity: 0, scale: 0.8 }}
                         animate={{ opacity: 1, scale: 1 }}
-                        transition={{ duration: 0.8, ease: "easeInOut", delay: 0.6 }}
+                        transition={{ duration: 0.7, ease: "easeInOut", delay: 0.5 }}
                     >
                         <div className="rounded-full p-3">
                             <Image
