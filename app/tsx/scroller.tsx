5ec84cdae61b10d574927d1f5b0d84728e455fa3
fabxx45 <fabian.hammer2709@gmail.com>
2025-03-07 22:03:48 +0100

    brightness change

--- app/tsx/scroller.tsx
+++ app/tsx/scroller.tsx
@@ -4,7 +4,7 @@ import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import { Autoplay, Navigation, EffectCoverflow } from 'swiper/modules'; // Removed Pagination from imports
+import { Autoplay, Navigation, EffectCoverflow } from 'swiper/modules';
 import Image from 'next/image';
 import LogoHAKHASITHTL from "../pictures/LogoHAKHASITHTL_gradient.png";
 import Uniqua from "../pictures/Uniqa_Österreich_logo.png";
@@ -14,51 +14,52 @@ const LogoSlider = () => {
     const alternatingLogos = logos.map((_, index) => (index % 2 === 0 ? LogoHAKHASITHTL : Uniqua));
 
     return (
-
-            <Swiper
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={3}
-                breakpoints={{
-                    640: { slidesPerView: 3 },
-                    768: { slidesPerView: 5 },
-                    1024: { slidesPerView: 7 },
-                    1280: { slidesPerView: 9 },
-                }}
-                spaceBetween={10}
-                loop={true}
-                autoplay={{
-                    delay: 2000,
-                    disableOnInteraction: false,
-                }}
-                navigation={false}
-                modules={[Autoplay, Navigation, EffectCoverflow]}
-                coverflowEffect={{
-                    rotate: 0,
-                    stretch: 0,
-                    depth: 200,
-                    modifier: 2.5,
-                    slideShadows: false,
-                }}
-                className="mySwiper"
-            >
-                {alternatingLogos.map((logo, index) => (
-                    <SwiperSlide key={index}>
-                        <div style={{ position: 'relative', width: '100%', height: '130px' }}>
-                            <Image
-                                src={logo}
-                                alt={`Logo ${index + 1}`}
-                                fill
-                                style={{ objectFit: 'contain' }}
-                                priority={index < 5}
-                                loading="eager"
-                            />
-                        </div>
-                    </SwiperSlide>
-                ))}
-            </Swiper>
+        <Swiper
+            effect={'coverflow'}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={3}
+            breakpoints={{
+                640: { slidesPerView: 3 },
+                768: { slidesPerView: 5 },
+                1024: { slidesPerView: 7 },
+                1280: { slidesPerView: 9 },
+            }}
+            spaceBetween={10}
+            loop={true}
+            autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+            }}
+            navigation={false}
+            modules={[Autoplay, Navigation, EffectCoverflow]}
+            coverflowEffect={{
+                rotate: 0,
+                stretch: 0,
+                depth: 200,
+                modifier: 2.5,
+                slideShadows: false,
+            }}
+            className="mySwiper"
+        >
+            {alternatingLogos.map((logo, index) => (
+                <SwiperSlide key={index}>
+                    <div style={{ position: 'relative', width: '100%', height: '130px' }}>
+                        <Image
+                            src={logo}
+                            alt={`Logo ${index + 1}`}
+                            fill
+                            style={{ objectFit: 'contain' }}
+                            priority={index < 5}
+                            loading="eager"
+                            className="swiper-slide-image"
+                        />
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
     );
 };
 
 export default LogoSlider;
+
