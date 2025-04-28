e56dd26adca153e58c8d6eea47571b0299689142
Imisstitlefight <kasim.mehinovic@sz-ybbs.ac.at>
2025-04-28 07:38:45 +0200

    Create page.tsx

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import Navbar from "@/app/tsx/navbar";
+
+export default function Datenschutz() {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Navbar />
+
+        </div>
+    )
+}
