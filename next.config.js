af79303b7aa75a76b76c352f6c755f94081e026c
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-05-02 14:44:34 +0200

    deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const nextConfig = {
+    output: 'export',
     devIndicators: {
         enabled: false,
     },
+};
+
+module.exports = nextConfig;
+
+module.exports = {
+    output: 'export',
 }
+
