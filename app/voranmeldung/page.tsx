ee94fd4a84e689fa723728a76a1d63231d6649be
fabxx45 <167181241+fabxx45@users.noreply.github.com>
2025-08-06 12:48:27 +0200

    change

--- app/voranmeldung/page.tsx
+++ app/voranmeldung/page.tsx
@@ -1,10 +1,14 @@
-'use client'
+'use client';
 
+<<<<<<< Updated upstream
 import React from "react";
+=======
+>>>>>>> Stashed changes
 import Navbar from "@/app/tsx/navbar";
-import Footer from "@/app/tsx/footer";
 import { motion } from "framer-motion";
+import Footer from "../tsx/footer";
 
+<<<<<<< Updated upstream
 export default function VoranmeldungPage() {
 
     const textVariants = {
@@ -15,10 +19,18 @@ export default function VoranmeldungPage() {
 
 
 
+=======
+const textVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeInOut" } }
+};
+>>>>>>> Stashed changes
 
+export default function voranmeldung() {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Navbar />
+<<<<<<< Updated upstream
             <motion.div
                 className="min-h-screen  flex items-center justify-center p-4"
                 style={{ fontFamily: "Arial" }}
@@ -30,5 +42,32 @@ export default function VoranmeldungPage() {
             </motion.div>
             <Footer/>
         </>
+=======
+            <div className="flex-grow pt-32">
+                <motion.header
+                    className="text-4xl mt-10 md:pl-32 text-center md:text-left"
+                    style={{ fontFamily: "Franklin Gothic Heavy" }}
+                    variants={textVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    Voranmeldung
+                </motion.header>
+
+                <motion.div
+                    className="flex flex-col md:flex-row justify-between px-6 mt-6 md:ml-32 text-lg leading-relaxed md:border-l-4 border-black pl-4 border-opacity-20 md:pr-32"
+                    style={{ fontFamily: "Arial" }}
+                    variants={textVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    <div className="max-w-full break-words text-base md:text-lg">
+                        Leider ist die Voranmeldung für dieses Jahr bereits geschlossen. Wir freuen uns aber auf nächstes Jahr!
+                    </div>
+                </motion.div>
+            </div>
+            <Footer />
+        </div>
+>>>>>>> Stashed changes
     );
 }
